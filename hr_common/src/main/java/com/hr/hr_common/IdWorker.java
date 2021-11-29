6f3ee90b1a8cfa6a8e1ea1b8869c4762ec1e0ba8
NULLPE <1989leosun@gmail.com>
2021-11-29 23:43:47 +0900

    idworkder

--- hr_common/src/main/java/com/hr/hr_common/IdWorker.java
+++ hr_common/src/main/java/com/hr/hr_common/IdWorker.java
@@ -1,5 +1,9 @@
 package com.hr.hr_common;
 
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
 /**
  * 1 bit 保留
  * 41 bit 时间戳 精确到毫秒 最多可以支持69年的跨度
@@ -8,4 +12,109 @@ package com.hr.hr_common;
  * 12 bit 毫秒内的计数器 每个节点每毫秒最多产生2的4096个ID
  */
 public class IdWorker {
+    private final static long twepoch = 1231231231234L;
+
+    private final static long workerIdBits = 5L;
+
+    private final static long datacenterIdBits = 5L;
+
+    private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
+
+    private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+
+    private final static long sequenceBits = 12L;
+
+    private final static long workerIdShift = sequenceBits;
+
+    private final static long datacenterIdShift = sequenceBits + workerIdBits;
+
+    private final static long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+
+    private final static long sequenceMask = -1L ^ (-1L << sequenceBits);
+
+    private static long lastTimestamp = -1L;
+
+    private long sequence = 0L;
+
+    private final long datacenterId;
+
+    private final long workerId;
+
+    public IdWorker() {
+        this.datacenterId = getDataCenterId(maxDatacenterId);
+        this.workerId = getMaxWorkerId(datacenterId, maxWorkerId);
+    }
+
+    public IdWorker(long workerId, long datacenterId) {
+        if (workerId > maxWorkerId || workerId < 0) {
+            throw new IllegalArgumentException(String.format("worker ID can't be greater than %d or less than 0", maxWorkerId));
+        }
+
+        if (datacenterId > maxDatacenterId || datacenterId < 0) {
+            throw new IllegalArgumentException(String.format("datacenter ID can't be greater than %d or less than 0", maxDatacenterId));
+        }
+
+        this.workerId = workerId;
+        this.datacenterId = datacenterId;
+    }
+
+    public synchronized long nextId() {
+        long timestamp = timeGen();
+        if (timestamp < lastTimestamp) {
+            throw new RuntimeException(String.format("Clock moved backwards. Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+        }
+
+        if (lastTimestamp == timestamp) {
+            sequence = (sequence + 1) & sequence;
+            if (sequence == 0) {
+                timestamp = tilNextmillis(lastTimestamp);
+            }
+        } else {
+            sequence = 0L;
+        }
+        lastTimestamp = timestamp;
+        return  ((timestamp - twepoch) << timestampLeftShift)
+                | (datacenterId << datacenterIdShift)
+                | (workerId << workerIdShift) | sequence;
+    }
+
+    private long tilNextmillis(final long lastTimestamp) {
+        long timestamp = this.timeGen();
+        while (timestamp <= lastTimestamp) {
+            timestamp = this.timeGen();
+        }
+        return timestamp;
+    }
+    private long timeGen() {
+        return System.currentTimeMillis();
+    }
+
+    protected static long getDataCenterId(long maxDatacenterId) {
+        long id = 0L;
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            if (network == null) {
+                id = 1L;
+            } else {
+                byte[] mac = network.getHardwareAddress();
+                id = ((0x000000FF00 & (long) mac[mac.length - 1])
+                        | (0x0000FF00 & (((long) mac[mac.length - 2]) << 8))) >> 6;
+                id = id % (maxDatacenterId + 1);
+            }
+        } catch (Exception e) {
+            System.out.println(" getDatacenterId: " + e.getMessage());
+        }
+        return id;
+    }
+
+    protected static long getMaxWorkerId(long datacenterId, long maxWorkerId) {
+        StringBuffer mpid = new StringBuffer();
+        mpid.append(datacenterId);
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        if (!name.isEmpty()) {
+            mpid.append(name.split("@")[0]);
+        }
+        return (mpid.toString().hashCode() & 0xffff) % (maxWorkerId + 1);
+    }
 }
