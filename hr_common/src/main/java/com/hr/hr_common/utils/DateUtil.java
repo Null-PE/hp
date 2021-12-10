1e3b2e3a8b7506fb0a4f257c71ca0b5c0ef0ab27
NULLPE <1989leosun@gmail.com>
2021-12-10 21:24:46 +0900

    update

--- hr_common/src/main/java/com/hr/hr_common/utils/DateUtil.java
+++ hr_common/src/main/java/com/hr/hr_common/utils/DateUtil.java
@@ -2,6 +2,7 @@ package com.hr.hr_common.utils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
@@ -35,6 +36,29 @@ public class DateUtil {
         SimpleDateFormat sdf = new SimpleDateFormat(pattern);
         return sdf.format(date);
     }
+
+    public static String getFirstDayOfMonth(int year, int month) {
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        int firstDay = cal.getMinimum(Calendar.DATE);
+        cal.set(Calendar.DAY_OF_MONTH, firstDay);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM.dd");
+        return sdf.format(cal.getTime());
+    }
+
+    public static String getLastDayOfMonth1(int year, int month) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month - 1);
+        int lastDay = cal.getActualMaximum(Calendar.DATE);
+        cal.set(Calendar.DAY_OF_MONTH, lastDay);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM.dd");
+        return sdf.format(cal.getTime());
+    }
+
+
 
 
 
